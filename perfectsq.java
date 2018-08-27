e7303e5df42b5469c92765c2aed56e2aef74e8f0
ramyachowdary4 <41936857+ramyachowdary4@users.noreply.github.com>
2018-08-27 11:40:47 +0530

    Create perfectsq.java

---
+++ perfectsq.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		double pro=a*b;
+		if(isperfectSquare(pro))
+		{
+			System.out.println("yes");
+		}
+		  else
+		  {
+		  	System.out.println("no");
+		  }
+		}
+		  static boolean isperfectSquare(double x)
+		  {
+		  	double sr = Math.sqrt(x);
+		  	return((sr - Math.floor(sr)) == 0);
+		  }
+	
+}
